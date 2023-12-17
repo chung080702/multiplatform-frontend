e35739da6f055aa3c9cf6eee0f0247be02320175
CTLong02 <chuthienlong02@gmail.com>
2023-12-17 20:44:57 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: AddEvent(),
+      home: RegisterPage(),
       onGenerateRoute: Routes.onGeneralRoute,
     );
   }
