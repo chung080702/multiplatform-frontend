1dc3b89419f68aa5bcd3286bd9adf956e9eb421a
CTLong02 <chuthienlong02@gmail.com>
2023-12-20 17:32:07 +0700

    lazy loading

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: RegisterPage(),
+      home: ViewEvent(),
       onGenerateRoute: Routes.onGeneralRoute,
     );
   }
