f2489a4cdaf702aaf7952897c2e474bd31d0f3a9
CTLong02 <chuthienlong02@gmail.com>
2023-12-25 15:19:41 +0700

    format date

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:multiplatform_app/screens/Login/login.index.dart';
 import 'package:multiplatform_app/screens/Register/register.index.dart';
 import 'package:multiplatform_app/screens/add_event/add_event.index.dart';
 import 'package:multiplatform_app/screens/view_event/view_event.index.dart';
 import 'package:multiplatform_app/general/routes/routes.dart';
 
-void main() {
+void main() async{
+  await initializeDateFormatting();
   runApp(MyApp());
 }
 
