ac2a03266fb22324921d350fcf92f08960eb3625
vinhhhhh47 <vinhduong412@gmail.com>
2024-01-08 12:53:29 +0700

    route group-detail from group-card-home

--- lib/widgets/group_card.index.dart
+++ lib/widgets/group_card.index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multiplatform_app/models/group.model.dart';
+import 'package:multiplatform_app/screens/Group/group.detail.dart';
 import 'package:multiplatform_app/utils/api_endpoint.dart';
 
 class GroupCard extends StatelessWidget {
@@ -9,45 +10,57 @@ class GroupCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.network(
-            ApiEndPoints.baseURL +
-                ApiEndPoints.fileEndPoints.getFile +
-                group.imageId,
-            height: 140,
-            width: 240,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(8, 3, 8, 0),
-            width: 240,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  group.name,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  '${group.memberNumber} thành viên',
-                  style: TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => GroupDetail(
+              groupId: group.id,
             ),
           ),
-        ],
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.network(
+              ApiEndPoints.baseURL +
+                  ApiEndPoints.fileEndPoints.getFile +
+                  group.imageId,
+              height: 140,
+              width: 240,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(8, 3, 8, 0),
+              width: 240,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    group.name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    '${group.memberNumber} thành viên',
+                    style: TextStyle(fontSize: 13, color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
